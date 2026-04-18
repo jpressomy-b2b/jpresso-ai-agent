50d6839c40c747e1a6149e9e8c693b44bb791695
jpressomy-b2b <jpresso.my@gmail.com>
2026-04-18 17:20:54 +0800

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 
 // WhatsApp Keys
 const ACCESS_TOKEN = process.env.META_ACCESS_TOKEN; 
-const phoneId = process.env.WA_PHONE_NUMBER_ID; 
+const phoneId = process.env.PHONE_NUMBER_ID; 
 
 // Instagram Keys
 const IG_ACCESS_TOKEN = process.env.IG_ACCESS_TOKEN; 
